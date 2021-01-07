9e55f3b358937751aaa067c4528b707e30a660e8
Tasheka Hamilton <tasheka.tech@gmail.com>
2021-01-07 15:13:24 -0500

    Add my_each_with_index method

--- enumerable_method.rb
+++ enumerable_method.rb
@@ -1,5 +1,4 @@
 def my_each(arr)
-    len = arr.length
     for name in arr do
         yield (name)
     end
@@ -7,3 +6,19 @@ end
 
 names = ["Amy", "John", "Joe"]
 my_each(names) {|a| puts "Hello #{a}"}
+
+def my_each_with_index(arr)
+    len = arr.length
+    i = 1
+    for item in arr do
+        if i <= len
+            if i.even?
+                yield (item)
+            end
+        end
+        i += 1
+    end
+end
+
+grocery_list = ["banana", "plum", "chocolate", "toiletries"]
+my_each_with_index(grocery_list) {|m| puts "Please get #{m}"}
