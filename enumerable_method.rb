1b53bf5ce429fdc8e258c4247bbd0262e8133d52
Ajanaku Temitope Grace <gracetemitope14@gmail.com>
2021-01-08 16:17:41 -0800

    complete my_all method

--- enumerable_method.rb
+++ enumerable_method.rb
@@ -1,73 +1,51 @@
 public def my_each(arr)
-    for name in arr do
-        yield (name)
+  arr.each do |name|
+    yield name
+  end
 end
 
-end
+names = %w[Amy John Joe]
 
-
-
-names = ["Amy", "John", "Joe"]
-
-my_each(names) {|a| puts "Hello #{a}"}
-
-
+my_each(names) { |a| puts "Hello #{a}" }
 
 def my_each_with_index(arr)
+  len = arr.length
 
-    len = arr.length
+  i = 1
 
-    i = 1
+  arr.each do |item|
+    yield item if i <= len && i.even?
 
-    for item in arr do
-
-        if i <= len
-
-            if i.even?
-
-                yield (item)
-
-            end
-
-        end
-
-        i += 1
-
-    end
-
+    i += 1
+  end
 end
 
+grocery_list = %w[banana plum chocolate toiletries]
 
-
-grocery_list = ["banana", "plum", "chocolate", "toiletries"]
-
-my_each_with_index(grocery_list) {|m| puts "Please get #{m}"}
-
+my_each_with_index(grocery_list) { |m| puts "Please get #{m}" }
 
 def my_select(arr)
   new_array = []
-    len = arr.length
-    for val in arr do  
-      unless val.is_a? Integer
-        new_array.push(val)
-      end
-    end
-    p new_array
+  len = arr.length
+  arr.each do |val|
+    new_array.push(val) unless val.is_a? Integer
   end
-    new_list = ["a", "b", "c", 3]
-  my_select(new_list)
-  
+  p new_array
+end
+new_list = ['a', 'b', 'c', 3]
+my_select(new_list)
 
-  def my_all(arr)
-      loop do 
-        if arr == Integer
-          p true
-        else
-          p false
-        end
-        break if true || false
-      end
-    end
-      new_element = [1, 3, "a"]
-    my_all(new_element)
-    
+def my_all(arr)
+  a = 0
+  len = arr.length
+  arr.each do |f|
+    a += 1 if f.is_a? Integer
+  end
+  if a == len
+    p true
+  else
+    p false
+  end
+end
+new_element = [1, 3, 2]
+my_all(new_element)
