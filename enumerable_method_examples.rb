56c43c0d731c65ed68d7483c384213860830c482
Tasheka Hamilton <tasheka.tech@gmail.com>
2021-01-11 10:22:50 -0500

    Edit my_each_with_index_method

--- enumerable_method_examples.rb
+++ enumerable_method_examples.rb
@@ -3,3 +3,7 @@ require "./enumerable_method"
 #Example for the my_each method
 names = ["Amy", "John", "Joe"]
 names.my_each {|name| p "Hello #{name}"}
+
+#Example for the my_each_with_index method
+grocery_list = ["bananas", "plums", "chocolates", "toiletries"]
+grocery_list.my_each_with_index {|grocery, index| p "Please get #{grocery}" if index.even?}
