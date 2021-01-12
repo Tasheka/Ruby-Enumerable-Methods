c52fc6c4e11b2cb7ae92b12f44f2b8b91c9d82d7
Tasheka Hamilton <tasheka.tech@gmail.com>
2021-01-12 15:44:46 -0500

    Add corrections

--- enumerable_method.rb
+++ enumerable_method.rb
@@ -1,87 +1,100 @@
 # Enumerable methods
 module Enumerable
   def my_each
-    p enum_for(:my_each) unless block_given?
+    return enum_for(:my_each) unless block_given?
     for value in self do
       yield value
     end
-    p self
+    return self
   end
 
   def my_each_with_index
-    p enum_for(:my_each_with_index) unless block_given?
+    return enum_for(:my_each_with_index) unless block_given?
     index = 0
     for value in self do
       yield(value, index)
       index += 1
     end
-    p self
+    return self
   end
 
   def my_select
-    p enum_for(:my_select) unless block_given?
+    return enum_for(:my_select) unless block_given?
     new_array = []
     for value in self do
       new_array.push(value) if yield value
     end
-    p new_array
+    return new_array
   end
 
-  def my_all?
+  def my_all?(arg = nil)
     conclusion = true
     for value in self do
       if !block_given? && !value
         conclusion = false
       elsif block_given? && !yield(value)
         conclusion = false
-      elsif (value.instance_of?(Class) || value.instance_of?(Module)) && !yield(value)
+      elsif (value.instance_of?(Class) || value.instance_of?(Module)) && !value.is_a?(arg)
         conclusion = false
-      elsif value.instance_of?(Regexp) && !yield(value)
+      elsif value.instance_of?(Regexp) && !arg.match(value)
+        conclusion = false
       end
   end
-    p conclusion
+    return conclusion
   end
 
-  def my_any?
+  def my_any?(arg = nil)
     conclusion = false
     for value in self do
       if !block_given? && value
         conclusion = true
       elsif block_given? && yield(value)
         conclusion = true
-      elsif (value.instance_of?(Class) || value.instance_of?(Module)) && yield(value)
+      elsif (value.instance_of?(Class) || value.instance_of?(Module)) && value.is_a?(arg)
         conclusion = true
-      elsif value.instance_of?(Regexp) && yield(value)
+      elsif arg.instance_of?(Regexp) && arg.match(value)
+        conclusion = true
       end
   end
     p conclusion
   end
 
-  def my_none?
+  def my_none?(arg = nil)
     conclusion = true
     for value in self do
       if !block_given? && value
         conclusion = false
       elsif block_given? && yield(value)
         conclusion = false
-      elsif (value.instance_of?(Class) || value.instance_of?(Module)) && yield(value)
+      elsif (arg.instance_of?(Class) || value.instance_of?(Module)) && value.is_a?(arg)
         conclusion = false
-      elsif value.instance_of?(Regexp) && yield(value)
+      elsif arg.instance_of?(Regexp) && arg.match(value)
+        conclusion = false
       end
-  end
-    p conclusion
+    end
+    return conclusion
   end
 
-  def my_count
+  def my_count(arg = nil)
     count = 0
-    for value in self do
-      count += 1 if yield(value)
-  end
+    if block_given?
+      for value in self do
+        count += 1 if yield(value)
+      end
+    elsif arg
+      for value in self do
+        count += 1 if arg == value
+      end
+    else
+      for value in self do
+        count += 1
+      end
+    end
     p count
   end
 
   def my_map(proc = nil)
-    p enum_for(:my_map) unless block_given?
+    return enum_for(:my_map) unless block_given?
     new_array = []
     if proc
       for value in self do
@@ -92,11 +105,11 @@ module Enumerable
         new_array.push(yield(value))
     end
     end
-    p new_array
+    return new_array
   end
 
   def my_inject(*input)
-    p LocalJumpError unless block_given? && !input.nil?
+    return LocalJumpError unless block_given? && !input.nil?
     if !input[0].nil? && input[1].nil?
       input1 = input[0]
       input2 = input[0]
@@ -114,4 +127,9 @@ module Enumerable
     end
     end
   end
+
+  def multiply_els(array)
+    array.my_inject { |value1, value2| value1 * value2 }
+  end
+
 end
