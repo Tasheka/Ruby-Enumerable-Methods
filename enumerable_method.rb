9277c227a59968afce7bf040562c16788c5a2ac4
Ajanaku Temitope Grace <gracetemitope14@gmail.com>
2021-01-12 17:36:06 -0800

    fix linters

--- enumerable_method.rb
+++ enumerable_method.rb
@@ -1,17 +1,15 @@
-module Enumerable 
-
+# Enumerable methods
+module Enumerable
   def my_each
     p enum_for(:my_each) unless block_given?
-    
     for value in self do
-      yield (value)
+      yield value
     end
     p self
   end
 
   def my_each_with_index
     p enum_for(:my_each_with_index) unless block_given?
-    
     index = 0
     for value in self do
       yield(value, index)
@@ -20,127 +18,100 @@ module Enumerable
     p self
   end
 
-
   def my_select
     p enum_for(:my_select) unless block_given?
-    
     new_array = []
     for value in self do
       new_array.push(value) if yield value
     end
     p new_array
   end
-  
-def my_all?
-  conclusion = true
 
-  for value in self do 
-    if !block_given? &&  !value
-      conclusion = false
-    elsif block_given? && !yield(value)
-      conclusion = false
-    elsif (value.instance_of?(Class) || value.instance_of?(Module)) && !yield(value)
-      conclusion  = false
-    elsif  value.instance_of?(Regexp)  && !yield(value)
-    end
-  end
-  p conclusion
-end
-
-def my_any?
-  conclusion = false
-  for value in self do 
-    if !block_given? &&  value
-      conclusion = true
-    elsif block_given? && yield(value)
-      conclusion = true
-    elsif (value.instance_of?(Class) || value.instance_of?(Module)) && yield(value)
-      conclusion  = true
-    elsif  value.instance_of?(Regexp)  && yield(value)
-    end
-  end
-  p conclusion
-end
-
-def my_none?
-  conclusion = true
-  for value in self do 
-    if !block_given? &&  value
-      conclusion = false
-    elsif block_given? && yield(value)
-      conclusion = false
-    elsif (value.instance_of?(Class) || value.instance_of?(Module)) && yield(value)
-      conclusion  = false
-    elsif  value.instance_of?(Regexp)  && yield(value)
-    end
-  end
-  p conclusion
-end
-    #output = true
-    #my_each(arr) do |num|
-     #   if num.is_a? Integer
-      #      output = false
-       # end
-    #end  
-    #if output == false
-     #   p false
-    #else
-     #   p true
-    #end
-#end
-
-def my_count
-  count = 0
-  for value in self do
-    count += 1 if yield(value)
-  end
-  p count
-end
-
-# def my_map(arr)
-#   new_array = []
-#   my_each(arr) do |item|
-#     new_array.push(item.upcase)
-#   end
-#   p new_array
-# end
-
-def my_map(proc = nil)
-  p enum_for(:my_map) unless block_given?
-
-  new_array = []
-  if proc
+  def my_all?
+    conclusion = true
     for value in self do
-      new_array.push(proc.call(value))
-    end
-  else
+      if !block_given? && !value
+        conclusion = false
+      elsif block_given? && !yield(value)
+        conclusion = false
+      elsif (value.instance_of?(Class) || value.instance_of?(Module)) && !yield(value)
+        conclusion = false
+      elsif  value.instance_of?(Regexp)  && !yield(value)
+      end
+  end
+    p conclusion
+  end
+
+  def my_any?
+    conclusion = false
     for value in self do
-      new_array.push(yield(value))
+      if !block_given? &&  value
+        conclusion = true
+      elsif block_given? && yield(value)
+        conclusion = true
+      elsif (value.instance_of?(Class) || value.instance_of?(Module)) && yield(value)
+        conclusion = true
+      elsif  value.instance_of?(Regexp)  && yield(value)
+      end
+  end
+    p conclusion
+  end
+
+  def my_none?
+    conclusion = true
+    for value in self do
+      if !block_given? && value
+        conclusion = false
+      elsif block_given? && yield(value)
+        conclusion = false
+      elsif (value.instance_of?(Class) || value.instance_of?(Module)) && yield(value)
+        conclusion = false
+      elsif  value.instance_of?(Regexp)  && yield(value)
+      end
+  end
+    p conclusion
+  end
+
+  def my_count
+    count = 0
+    for value in self do
+      count += 1 if yield(value)
+  end
+    p count
+  end
+
+  def my_map(proc = nil)
+    p enum_for(:my_map) unless block_given?
+    new_array = []
+    if proc
+      for value in self do
+        new_array.push(proc.call(value))
+    end
+    else
+      for value in self do
+        new_array.push(yield(value))
+    end
+    end
+    p new_array
+  end
+
+  def my_inject(*input)
+    p LocalJumpError unless block_given? && !input.nil?
+    if input.length ==  1 && input[0].is_a?(Integer)
+      input1 = input[0]
+      input2 = input[0]
+    elsif input.length > 1 && input[0].is_a?(Integer) && input[1].is_a?(Integer)
+      input1 = input[0]
+      input2 = input[1]
+    end
+    if input2
+      for value in self do
+        input2.send.call(input1, value)
+    end
+    elsif !input2
+      for value in self do
+        yield(input1,value)
+    end
     end
   end
-  p new_array
-end
-
-
-def my_inject(*input)
-  p LocalJumpError unless block_given? && !input.nil?
-
- if input.length ==  1 && input[0].is_a?(Integer)
-    input1 = input[0]
-    input2 = input[0]
-  elsif input.length > 1 && input[0].is_a?(Integer) && input[1].is_a?(Integer)
-    input1 = input[0]
-    input2 = input[1]
-  end
-  if input2
-    for value in self do
-    input2.send.call(input1, value)
-    end
-  elsif !input2
-    for value in self do
-      yield(input1,value)
-    end
-  end
-end
-  
 end
--- enumerable_method_examples.rb
+++ enumerable_method_examples.rb
@@ -1,39 +1,39 @@
-require "./enumerable_method"
+# Examples
+require './enumerable_method'
 
-#Example for the my_each method
-names = ["Amy", "John", "Joe"]
-names.my_each {|name| p "Hello #{name}"}
+# Example for the my_each method
+names = %w[ 'Amy', 'John', 'Joe' ]
+names.my_each { |name| p "Hello #{name}" }
 
-#Example for the my_each_with_index method
-grocery_list = ["bananas", "plums", "chocolates", "toiletries"]
-grocery_list.my_each_with_index {|grocery, index| p "Please get #{grocery}" if index.even?}
+# Example for the my_each_with_index method
+grocery_list = %w[ 'bananas', 'plums', 'chocolates', 'toiletries' ]
+grocery_list.my_each_with_index { |grocery, index| p "Please get #{grocery}" if index.even?}
 
-#Example for the my_select method
-letters = ["a", "b", "c", 3]
-letters.my_select {|letter| letter.is_a? String}
+# Example for the my_select method
+letters = ['a', 'b', 'c', 3]
+letters.my_select { |letter| letter.is_a? String}
 
-#Example for the my_all method
-numbers  = [9, 10, 12, 20]
+# Example for the my_all method
+numbers = [9, 10, 12, 20]
 numbers.my_all? { |number| number.is_a? Integer }
 
-#Example for the my_any method
-numbers  = [9, 10, 12, "Tasheka", "lilo"]
+# Example for the my_any method
+numbers  = [9, 10, 12, 'Tasheka', 'lilo']
 numbers.my_any? { |number| number.is_a? Integer }
 
-#Example for the my_none method
-words  = ["Tasheka", "lilo", "cat"]
+# Example for the my_none method
+words = %w['Tasheka', 'lilo', 'cat']
 words.my_none? { |number| number.is_a? Integer }
 
-#Example for the my_count method
-new_letters = ['a', 'g', 'h', 'i']
+# Example for the my_count method
+new_letters = %w['a', 'g', 'h', 'i']
 new_letters.my_map  {|letter| letter.upcase}
 elements = [1, 2, 3, 4]
-elements.my_map{ |i| i * i }
-                                                                              
+elements.my_map { |i| i * i }
 
-#Example for the my_map method
-new_letters = ['a', 'g', 'h', 'i']
-new_letters.my_map  {|letter| letter.upcase}
+# Example for the my_map method
+new_letters = %w['a', 'g', 'h', 'i']
+new_letters.my_map{ | letter | letter.upcase }
 
-elements = ["a", "b", "c"]
+elements = %w['a', 'b', 'c']
 elements.my_map proc{|value| value.upcase}
